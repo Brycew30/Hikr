88d007314897c8e707b1068acbbb5f51edb0a1de
Brycew30 <brycew30@gmail.com>
2020-09-06 16:52:13 -0500

    add belongs_to, nested attributes, and validations to trail class

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,10 @@
 class Trail < ApplicationRecord
+  belongs_to :location
+  accepts_nested_attributes_for :location
+
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :length, presence: true
+  validates :description, presence: true
+  # validates :location, presence: true
 end
