d2ec79b54e673ef4afb0f3641195c4ececdf9694
Brycew30 <brycew30@gmail.com>
2020-09-08 09:53:42 -0500

    create home and welcome methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def home
   end
+
+  def welcome
+    redirect_to home_path if user_signed_in?
+  end
     protected
     def configure_permitted_parameters
       added_attrs = %i[username email password password_confirmation remember_me]
