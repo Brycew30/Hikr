7f02f5d27e52dc513161f6330ac6c05cd26d7beb
Brycew30 <brycew30@gmail.com>
2020-09-08 09:53:05 -0500

    make welcome root route and home for signed in users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   end
   resources :reviews
   resources :locations
-  root to: 'application#home'
+  root to: 'application#welcome'
+  get 'home', to: 'application#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
