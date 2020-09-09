9f29c502ab45c365b11d6d95cfe3e4d25b24ab22
Brycew30 <brycew30@gmail.com>
2020-09-09 13:42:24 -0500

    add has many trails relationship to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
 class Location < ApplicationRecord
   has_many :trails
   validates :name, presence: true
+
+  def location_attributes(location_attributes)
+    location_name = location_attributes["name"]
+    # unless location_name.blank?
+    #   location = Location.find_or_create_by(name: location_name)
+    #   self.location = locatino
+    # end
+  end
 end
