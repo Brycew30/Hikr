a16a351d40fba0dde8404d1a81fcfb52d9185a6c
Brycew30 <brycew30@gmail.com>
2020-09-09 17:34:47 -0500

    cleaned up unused nested attributes helper

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,10 +1,11 @@
 class Trail < ApplicationRecord
+  has_many :reviews
+  has_many :users, through: :reviews
   belongs_to :location
-  accepts_nested_attributes_for :location
 
   validates :name, presence: true
   validates :path_type, presence: true
   validates :length, presence: true
   validates :description, presence: true
-  # validates :location, presence: true
+  validates :location, presence: true
 end
