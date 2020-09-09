10fe27494c96b2a9d055f9ef18c6fa8163f49dfe
Brycew30 <brycew30@gmail.com>
2020-09-09 08:47:01 -0500

    update User relationships and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,24 +9,24 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
 
- # validates :username, presence: true, uniqueness: { case_sensitive: false }
- #        validate :validate_username
- #        attr_writer :login
- #
- #        def login
- #            @login || username || email
- #        end
- #
- #        def validate_username
- #            errors.add(:username, :invalid) if User.where(email: username).exists?
- #        end
- #
- #        def self.find_for_database_authentication(warden_conditions)
- #            conditions = warden_conditions.dup
- #            if login = conditions.delete(:login)
- #                where(conditions.to_hash).where(['lower(username) = :value OR lower(email) = :value', {value: login.downcase}]).first
- #            elsif conditions.key?(:username) || conditions.key?(:email)
- #                where(conditions.to_h).first
- #            end
- #        end
+ validates :username, presence: true, uniqueness: { case_sensitive: false }
+        validate :validate_username
+        attr_writer :login
+
+        def login
+            @login || username || email
+        end
+
+        def validate_username
+            errors.add(:username, :invalid) if User.where(email: username).exists?
+        end
+
+        def self.find_for_database_authentication(warden_conditions)
+            conditions = warden_conditions.dup
+            if login = conditions.delete(:login)
+                where(conditions.to_hash).where(['lower(username) = :value OR lower(email) = :value', {value: login.downcase}]).first
+            elsif conditions.key?(:username) || conditions.key?(:email)
+                where(conditions.to_h).first
+            end
+        end
 end
