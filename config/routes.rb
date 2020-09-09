d24f09a5616291d17e67a1494883ab32d51b3eb1
Brycew30 <brycew30@gmail.com>
2020-09-09 15:14:01 -0500

    organize routes with spacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   resources :trails do
     resources :reviews, only: [:new, :show, :index]
   end
+
   resources :reviews
+
   get '/locations', to: 'application#welcome'
+
   root to: 'application#welcome'
+
   get 'home', to: 'application#home'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
