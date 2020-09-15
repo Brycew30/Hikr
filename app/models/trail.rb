52e1a11847a4b0c23784c00f5aeff1063e604d76
Brycew30 <brycew30@gmail.com>
2020-09-15 06:54:44 -0500

    add required * to trail location form

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -7,7 +7,7 @@ class Trail < ApplicationRecord
   validates :path_type, presence: true
   validates :length, presence: true
   validates :description, presence: true
-  validates :location, presence: true
+  validates :location_id, presence: true
 
   scope :search, -> (query) {where("name LIKE ?", "%" + query + "%") }
   scope :alphabetical_order, -> {order(:name)}
